0421d356df91e21e61c43fd22f75d0c5d3dd8b21
kj16609 <bigskyman02@gmail.com>
2024-01-24 05:48:56 -0500

    Fix tracy

--- cmake_modules/dependencies/tracy.cmake
+++ cmake_modules/dependencies/tracy.cmake
@@ -1,5 +1,13 @@
-#if (WIN32)
+
+
+string(TOUPPER ${CMAKE_BUILD_TYPE} CMAKE_UPPER_BUILD_TYPE)
+
+message("-- Cmake upper: ${CMAKE_UPPER_BUILD_TYPE}")
+
+if (CMAKE_UPPER_BUILD_TYPE STREQUAL "DEBUG")
+	set(TRACY_ENABLE ON)
+	set(TRACY_ON_DEMAND ON)
+endif ()
+
 add_subdirectory(${CMAKE_SOURCE_DIR}/dependencies/tracy)
-#else ()
-#find_package(tracy REQUIRED)
-#endif ()
+
