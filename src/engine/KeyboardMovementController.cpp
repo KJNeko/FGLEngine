48eab9398816f94e803089da3349016edc21ceba
kj16609 <bigskyman02@gmail.com>
2024-01-24 07:12:00 -0500

    Fixes camera wip-around bug

--- src/engine/KeyboardMovementController.cpp
+++ src/engine/KeyboardMovementController.cpp
@@ -9,6 +9,20 @@
 namespace fgl::engine
 {
 
+	void setCursorPos( GLFWwindow* window, const glm::vec2 pos )
+	{
+		glfwSetCursorPos( window, pos.x, pos.y );
+	}
+
+	glm::vec2 getCursorPos( GLFWwindow* window )
+	{
+		//Take cursor input
+		double xpos { 0.0f };
+		double ypos { 0.0f };
+		glfwGetCursorPos( window, &xpos, &ypos );
+		return { xpos, ypos };
+	}
+
 	void KeyboardMovementController::moveInPlaneXZ( GLFWwindow* window, float dt, fgl::engine::GameObject& target )
 	{
 		glm::vec3 rotate { 0.0f };
@@ -19,37 +33,52 @@ namespace fgl::engine
 		if ( glfwGetKey( window, key_mappings.look_down ) == GLFW_PRESS ) rotate.x -= 1.f;
 
 		static bool cursor_enabled { true };
+		static bool cursor_restored { false };
+		static glm::vec2 stored_cursor_pos;
 		static std::chrono::time_point last_pressed { std::chrono::steady_clock::now() };
 
 		if ( glfwGetKey( window, GLFW_KEY_ESCAPE ) == GLFW_PRESS )
 		{
 			const auto now { std::chrono::steady_clock::now() };
 			using namespace std::chrono_literals;
-			if ( now - last_pressed > 1s )
+			if ( now - last_pressed > 500ms )
 			{
 				cursor_enabled = !cursor_enabled;
 				last_pressed = now;
 			}
 		}
 
-		if ( cursor_enabled )
+		if ( cursor_enabled && !cursor_restored )
+		{
 			glfwSetInputMode( window, GLFW_CURSOR, GLFW_CURSOR_NORMAL );
-		else
+			if ( !cursor_restored )
+			{
+				setCursorPos( window, stored_cursor_pos );
+				std::cout << "Restored cursor pos: " << stored_cursor_pos.x << "x" << stored_cursor_pos.y << std::endl;
+				cursor_restored = true;
+			}
+		}
+		else if ( !cursor_enabled && cursor_restored )
+		{
+			stored_cursor_pos = getCursorPos( window );
+			std::cout << "Stored cursor pos: " << stored_cursor_pos.x << "x" << stored_cursor_pos.y << std::endl;
+			cursor_restored = false;
 			glfwSetInputMode( window, GLFW_CURSOR, GLFW_CURSOR_DISABLED );
+			setCursorPos( window, { 0, 0 } );
+		}
 
 		if ( !cursor_enabled )
 		{
-			//Take cursor input
-			double xpos { 0.0f };
-			double ypos { 0.0f };
-			glfwGetCursorPos( window, &xpos, &ypos );
+			const auto pos { getCursorPos( window ) };
+			const auto xpos { pos.x };
+			const auto ypos { pos.y };
 
 			target.transform.rotation.y +=
 				static_cast< float >( ( xpos * 0.006 ) * static_cast< double >( look_speed ) );
 			target.transform.rotation.x -=
 				static_cast< float >( ( ypos * 0.006 ) * static_cast< double >( look_speed ) );
 
-			glfwSetCursorPos( window, 0, 0 );
+			setCursorPos( window, { 0, 0 } );
 		}
 		else
 		{
