022cdc681e0cb70d481a6bdcdf74ac94e6eac6a8
kj16609 <bigskyman02@gmail.com>
2024-07-23 00:39:22 -0400

    Switch glfw to use the module to allow renderdoc to work

--- cmake_modules/dependencies/glfw.cmake
+++ cmake_modules/dependencies/glfw.cmake
@@ -1,5 +1,9 @@
 
-if (WIN32)
+
+set(GLFW_BUILD_X11 ON)
+set(GLFW_BUILD_WAYLAND OFF)
+
+#if (WIN32)
 	if (DEFINED ENV{GLFW_PATH})
 		message("-- GLFW_PATH defined as: $ENV{GLFW_PATH}.")
 		list(APPEND CMAKE_PREFIX_PATH $ENV{GLFW_PATH})
@@ -8,6 +12,9 @@ if (WIN32)
 		message("-- GLFW_PATH not defined. Using submodule instead")
 		add_subdirectory(${CMAKE_SOURCE_DIR}/dependencies/glfw3)
 	endif ()
-else ()
-	find_package(glfw3 REQUIRED)
-endif ()
+#else ()
+#	find_package(glfw3 REQUIRED)
+#endif ()
+
+set(GLFW_BUILD_X11 ON)
+set(GLFW_BUILD_WAYLAND OFF)
