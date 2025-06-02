816397c165c9eeba630e0b36984f5238f7378eea
kj16609 <bigskyman02@gmail.com>
2025-06-02 15:40:11 -0400

    Add support for c++ modules for later on

--- cmake_modules/dependencies/vulkan.cmake
+++ cmake_modules/dependencies/vulkan.cmake
@@ -3,4 +3,18 @@ if (NOT Vulkan_FOUND)
 	error("Vulkan not found")
 endif ()
 
+#add_library(VulkanHppModule)
+#
+#target_include_directories(VulkanHppModule
+#		PRIVATE
+#		"${Vulkan_INCLUDE_DIR}"
+#)
+#
+#target_sources(VulkanHppModule PUBLIC
+#		FILE_SET CXX_MODULES
+#		BASE_DIRS ${Vulkan_INCLUDE_DIR}
+#		FILES ${Vulkan_INCLUDE_DIR}/vulkan/vulkan.cppm
+#)
+#target_compile_definitions(VulkanHppModule PUBLIC VULKAN_HPP_DISPATCH_LOADER_DYNAMIC=1 VULKAN_HPP_NO_STRUCT_CONSTRUCTORS)
+#
 message("Vulkan include: ${Vulkan_INCLUDE_DIR}")
